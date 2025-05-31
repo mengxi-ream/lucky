9890a4cf784bc946776281114a250174b9aaa9eb
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-30 17:12:57 -0700

    solution: uva-272

--- 2025-05/30/uva-272-tex-quotes.py
+++ 2025-05/30/uva-272-tex-quotes.py
@@ -4,7 +4,18 @@ import os
 LOCAL = os.getenv("LOCAL", "false").lower() == "true"
 
 def main():
-  print("Hello world", LOCAL)
+    left = True
+    for line in sys.stdin:
+        line = line.rstrip('\n')  # 去掉行末的换行符
+        parsedLine = ""
+        for c in line:
+          if c == '"':
+            parsedLine += '``' if left else '\'\''
+            left = not left
+          else:
+            parsedLine += c
+        print(parsedLine)
+              
 
 if __name__ == "__main__":
   if LOCAL:
